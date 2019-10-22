ac098e2f2c61b164218b4919ec4b0b0b91db29f2
liuce <liuce@liuce.lan>
2019-10-22 23:43:00 +0800

    modified drools client

--- src/main/scala/com/hello/riskControl/drools/DroolsUtil.java
+++ src/main/scala/com/hello/riskControl/drools/DroolsUtil.java
@@ -1,5 +1,6 @@
 package com.hello.riskControl.drools;
 
+import com.hello.constants.Constants;
 import com.hello.utils.GwDataStruct;
 import com.hello.utils.GwDataStructService;
 import org.kie.api.KieBase;
@@ -8,107 +9,36 @@ import org.kie.api.KieServices;
 import org.kie.api.builder.KieBuilder;
 import org.kie.api.builder.KieFileSystem;
 import org.kie.api.builder.Results;
-import org.kie.api.io.ResourceType;
+import org.kie.api.conf.EqualityBehaviorOption;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 import org.kie.api.runtime.rule.FactHandle;
-//import org.kie.internal.KnowledgeBase;
-//import org.kie.internal.KnowledgeBaseFactory;
-
-import org.drools.core.impl.InternalKnowledgeBase;
-import org.drools.core.impl.KnowledgeBaseFactory;
-import org.drools.core.impl.KnowledgeBaseFactoryServiceImpl;
-
-
-import org.kie.internal.builder.KnowledgeBuilder;
-import org.kie.internal.builder.KnowledgeBuilderError;
-import org.kie.internal.builder.KnowledgeBuilderErrors;
-import org.kie.internal.builder.KnowledgeBuilderFactory;
+import org.kie.internal.conf.MaxThreadsOption;
 import org.kie.internal.conf.MultithreadEvaluationOption;
-import org.kie.internal.io.ResourceFactory;
 
 import java.io.UnsupportedEncodingException;
 import java.sql.*;
+import java.util.HashMap;
+import java.util.Map;
+
+//import org.kie.internal.KnowledgeBase;
+//import org.drools.core.impl.KnowledgeBaseFactory;
 
 
 public class DroolsUtil {
 //    static StatefulKnowledgeSession kieSession;
-    static KieSession kieSessions;
-    static KieContainer kieContainer;
-    static KieBase kieBase;
-
-    public DroolsUtil() throws ClassNotFoundException, SQLException, UnsupportedEncodingException{
-        Class.forName("org.postgresql.Driver");
-//        Connection connection= DriverManager.getConnection("jdbc:postgresql://10.111.40.148:5432/ticket","dbuser", "dbuser@cys");
-        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ticket", "dbuser", "dbuser@cys");
-
-        String sql = "select * from my_rule";
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
-        String rule = null;
-        while (resultSet.next()) {
-            rule = resultSet.getString("rule");
-            System.out.println(rule);
-        }
-        statement.close();
-        connection.close();
-        KnowledgeBuilder kb = KnowledgeBuilderFactory.newKnowledgeBuilder();
-        kb.add(ResourceFactory.newByteArrayResource(rule.getBytes("utf-8")), ResourceType.DRL);
-        KnowledgeBuilderErrors errors = kb.getErrors();
-        for (KnowledgeBuilderError error : errors) {
-            System.out.println(error);
-        }
-        InternalKnowledgeBase kBase = KnowledgeBaseFactory.newKnowledgeBase();
-        kBase.addPackages(kb.getKnowledgePackages());
+    static volatile KieSession kieSessions;
+    static volatile KieContainer kieContainer;
+    static volatile KieBase kieBase;
 
 
-        kieSessions = kBase.newKieSession();
-    }
-
-    public static KieSession getKieSession() throws  ClassNotFoundException, SQLException, UnsupportedEncodingException {
-        if(kieSessions == null){
-            new DroolsUtil();
-        }
-        return kieSessions;
-    }
-
-    public static KieSession getKieSessionDynamic() throws  ClassNotFoundException, SQLException, UnsupportedEncodingException{
-        Class.forName("org.postgresql.Driver");
-        Connection connection= DriverManager.getConnection("jdbc:postgresql://10.111.40.148:5432/ticket","dbuser", "dbuser@cys");
-//        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ticket", "dbuser", "dbuser@cys");
-//            System.out.println("是否成功连接pg数据库"+connection);
-        String sql = "select * from my_rule";
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(sql);
-        String rule = null;
-        while (resultSet.next()) {
-            rule = resultSet.getString("rule");
-//            System.out.println(rule);
-        }
-        KnowledgeBuilder kb = KnowledgeBuilderFactory.newKnowledgeBuilder();
-        kb.add(ResourceFactory.newByteArrayResource(rule.getBytes("utf-8")), ResourceType.DRL);
-        KnowledgeBuilderErrors errors = kb.getErrors();
-        for (KnowledgeBuilderError error : errors) {
-            System.out.println(error);
-        }
-        InternalKnowledgeBase kBase = KnowledgeBaseFactory.newKnowledgeBase();
-        kBase.addPackages(kb.getKnowledgePackages());
-        statement.close();
-        connection.close();
-        kieSessions = kBase.newKieSession();
-
-        return kieSessions;
-    }
-
-
-
-    public static boolean initKieSession(String newRule) {
-        KieServices kieServices = KieServices.Factory.get();
-        KieFileSystem kfs = kieServices.newKieFileSystem();
-//        kfs.write("src/main/resources/rules/rules.drl", DroolsUtil_Dynamic.RULE.getBytes());
-        if(!newRule.equals(RULE)) {
-            RULE = newRule;
-            kfs.write("src/main/resources/rules/rules.drl", newRule.getBytes());
+    public static boolean initKieSession(Map<String,String> newRule) {
+        if(!CompareMap.compareMapByEntrySet(RULE,newRule)){
+            KieServices kieServices = KieServices.Factory.get();
+            KieFileSystem kfs = kieServices.newKieFileSystem();
+            for (Map.Entry<String, String> entry : newRule.entrySet()) {
+                kfs.write("src/main/resources/rules/"+entry.getKey()+".drl", entry.getValue().getBytes());
+            }
             KieBuilder kieBuilder = kieServices.newKieBuilder(kfs).buildAll();
             Results results = kieBuilder.getResults();
             if (results.hasMessages(org.kie.api.builder.Message.Level.ERROR)) {
@@ -116,13 +46,18 @@ public class DroolsUtil {
                 System.out.println("-----------语法解析报错，drools规则更新无效------------------");
                 return false;
             }
-            KieContainer kieContainer = kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
+            kieContainer = kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
 
             KieBaseConfiguration kieBaseConf = kieServices.newKieBaseConfiguration();
             kieBaseConf.setOption(MultithreadEvaluationOption.YES);
-            
-            KieBase kieBase = kieContainer.newKieBase(kieBaseConf);
+            kieBaseConf.setOption(MaxThreadsOption.get(100));
+            kieBaseConf.setOption(EqualityBehaviorOption.EQUALITY);
+//            kieBaseConf.setOption(EventProcessingOption.STREAM);
+            kieBase = kieContainer.newKieBase(kieBaseConf);
+//            KieSessionConfiguration kieSessionConfiguration = kieServices.newKieSessionConfiguration();
+//            kieSessionConfiguration.setOption();
             kieSessions = kieBase.newKieSession();
+            RULE = newRule;
             return true;
         }
         else {
@@ -130,49 +65,39 @@ public class DroolsUtil {
         }
     }
 
-    public static String RULE = "";
+    public static Map RULE = new HashMap();
 
     public static void updateRule(){
         try {
-            String newRule = getNewRule();
-            if (newRule != null && newRule.length() != 0 && (!newRule.equals(RULE))) {
+            Map newRule = getNewRule();
+            if (newRule != null ) {
                 boolean isGoodRule=initKieSession(newRule);
                 if(isGoodRule){
                     GwDataStructService gwDataStructService = new GwDataStructService();
 
                     try{
                         Class global = kieSessions.getGlobal("gwDataStructService").getClass();
-                        System.out.println(global);
                     }
                     catch (Exception e){
                         kieSessions.setGlobal("gwDataStructService", gwDataStructService);
                     }
-
-                    System.out.println("RULE被成功更新");
-                    System.out.println("session被成功更新创建");
                 }
             }
-            System.out.println(kieSessions);
-//            return kieSessions;
         } catch (Exception e) {
             e.printStackTrace();
         }
-//        return kieSessions;
     }
 
-    public static String getNewRule() throws ClassNotFoundException, SQLException, UnsupportedEncodingException{
+    public static Map getNewRule() throws ClassNotFoundException, SQLException, UnsupportedEncodingException{
         Class.forName("org.postgresql.Driver");
-        Connection connection= DriverManager.getConnection("jdbc:postgresql://116.62.120.210:5432/ticket","dbuser", "dbuser@cys");
-//        Connection connection= DriverManager.getConnection("jdbc:postgresql://10.111.40.148:5432/ticket","dbuser", "dbuser@cys");
-//        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/ticket", "dbuser", "dbuser@cys");
-//            System.out.println("是否成功连接pg数据库"+connection);
-        String sql = "select * from my_rule";
+        Connection connection= DriverManager.getConnection(Constants.POSTGREURL(),Constants.POSTGREUSER(), Constants.POSTGREPASSWORD());
+//        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/riskcontrol", "dbuser", "dbuser@cys");
+        String sql = Constants.POSTGRESQL();
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery(sql);
-        String rule = null;
+        Map rule = new HashMap();
         while (resultSet.next()) {
-            rule = resultSet.getString("rule");
-//            System.out.println(rule);
+            rule.put( resultSet.getString("rulename"), resultSet.getString("rulecontent"));
         }
         statement.close();
         connection.close();
@@ -180,10 +105,50 @@ public class DroolsUtil {
     }
 
     public static void execRule(GwDataStruct gwDataStruct){
-
         FactHandle ss = kieSessions.insert(gwDataStruct);
         kieSessions.fireAllRules();
         kieSessions.delete(ss);
+    }
+
+    public static KieSession getKieSessions(){
+        try {
+            Map<String,String> newRule = getNewRule();
+            if (newRule != null ) {
+
+                KieServices kieServices = KieServices.Factory.get();
+                KieFileSystem kfs = kieServices.newKieFileSystem();
+                for (Map.Entry<String, String> entry : newRule.entrySet()) {
+                    kfs.write("src/main/resources/rules/"+entry.getKey()+".drl", entry.getValue().getBytes());
+                }
+                KieBuilder kieBuilder = kieServices.newKieBuilder(kfs).buildAll();
+                Results results = kieBuilder.getResults();
+                if (results.hasMessages(org.kie.api.builder.Message.Level.ERROR)) {
+                    System.out.println(results.getMessages());
+                    System.out.println("-----------语法解析报错，drools规则更新无效------------------");
+                }
+                kieContainer = kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
+
+                KieBaseConfiguration kieBaseConf = kieServices.newKieBaseConfiguration();
+                kieBaseConf.setOption(MultithreadEvaluationOption.YES);
+                kieBaseConf.setOption(MaxThreadsOption.get(10));
+
+                kieBase = kieContainer.newKieBase(kieBaseConf);
+               KieSession kieSession = kieBase.newKieSession();
+
+                GwDataStructService gwDataStructService = new GwDataStructService();
+
+                try{
+                    Class global = kieSession.getGlobal("gwDataStructService").getClass();
+                }
+                catch (Exception e){
+                    kieSession.setGlobal("gwDataStructService", gwDataStructService);
+                }
+                return kieSession;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
